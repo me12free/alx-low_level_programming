5537501d13da4dc7ddefe2ece207efb4d89e8d31
me12free <johnewoi72@gmail.com>
2023-02-21 15:06:29 -0800

    5. Sign-Write a function that prints the sign of a number.

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,12 @@
 /**
  * _islower - function to print lowercase
  *
+ * @c: parameter to be printed
  *
- * 0c: parameter to be printed
- *
- *
- * Return: 1 if it is a lowercase and 0 otherwise
+ * Return: 1 if it is a lowercase
+ * and 0 otherwise
  */
+
 int _islower(int c)
 {
 	if (c >= 97 && c <= 122)
