fcfbf891ee6d3aae81ffa90d15186ae0b097e78a
me12free <johnewoi72@gmail.com>
2023-02-21 12:36:53 -0800

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_alphabet -function that prints a-z
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char x = 'a';
+
+	while (x <= 'z')
+	{
+		_putchar(x);
+		x++;
+		_putchar('\n');
+	}
+}
