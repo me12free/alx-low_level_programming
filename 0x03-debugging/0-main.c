8dc457b598ffea92ba676b0f06b421f12a83a976
me12free <johnewoi72@gmail.com>
2023-02-22 09:35:03 -0800

    C debugging Task 0 to Task 3

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include "main.c"
+/**
+ * main - test function that prints of 0 is positive or negative
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
