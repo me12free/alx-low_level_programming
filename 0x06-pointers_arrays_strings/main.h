e300afcd31b015b9f1a26e3cca900ec382611eac
me12free <johnewoi72@gmail.com>
2023-03-02 10:44:54 -0800

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,5 +12,5 @@ char *rot13(char *);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
-
+int _putchar(char c);
 #endif
