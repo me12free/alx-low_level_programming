6a6209f73ed67b5afe49481f17e2fb5f30671108
me12free <johnewoi72@gmail.com>
2023-03-06 09:49:35 -0800

    a function that locates a character in a string.

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -14,18 +14,18 @@
 
 void print_diagsums(int *a, int size)
 {
-        int counter1, counter2, sum1, sum2;
+	int counter1, counter2, sum1, sum2;
 
-        sum1 = 0;
-        sum2 = 0;
+	sum1 = 0;
+	sum2 = 0;
 
-        for (counter1 = 0; counter1 < size; counter1++)
-        {
-                sum1 += a[(size + 1) * counter1];
-        }
-        for (counter2 = 0; counter2 < size; counter2++)
-        {
-                sum2 += a[(size - 1) * (counter2 + 1)];
-        }
-        printf("%d, %d\n", sum1, sum2);
+	for (counter1 = 0; counter1 < size; counter1++)
+	{
+		sum1 += a[(size + 1) * counter1];
+	}
+	for (counter2 = 0; counter2 < size; counter2++)
+	{
+		sum2 += a[(size - 1) * (counter2 + 1)];
+	}
+	printf("%d, %d\n", sum1, sum2);
 }
