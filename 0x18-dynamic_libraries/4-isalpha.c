476f44eeffc3f5670669c7c1e18e5403219e16cf
me12ree <johnewoi72@gmail.com>
2024-01-15 11:40:43 -0800

    bugs

---
+++ 0x18-dynamic_libraries/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha -  function that checks for alphabetic character.
+ *
+ * @c: parametr to checked
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
+ * and 0 otherwise
+ */
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+		return (1);
+	else
+		return (0);
+}
